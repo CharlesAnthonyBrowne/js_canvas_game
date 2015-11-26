df295bd85d4a31841ff6eb1a26689fd85585127f
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-11-26 17:28:33 +0000

    js assets

--- main.js
+++ main.js
@@ -72,3 +72,25 @@ var update = function (modifier) {
 		reset();
 	}
 };
+
+// Draw everything
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+};
